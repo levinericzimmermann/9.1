dd921cf3430d8dbdf03d248b1b29c7986c778eee
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-10-22 16:03:19 +0200

    Prefer longer duration ranges
    Previously it was too short / too stressful for indented situation.
    Now it's better.

--- mutwo.pages/mutwo/pages_converters/constants.py
+++ mutwo.pages/mutwo/pages_converters/constants.py
@@ -8,10 +8,12 @@ SCORE_TEMPLATE_PATH = f"{TEMPLATES_PATH}/score.tex.j2"
 PAGE_COVER_TEMPLATE_PATH = f"{TEMPLATES_PATH}/page-cover.tex.j2"
 
 # Page creation
-MINIMA_DURATION_GENERATOR_ENVELOPE = core_events.Envelope([[0, 0.5], [4, 1], [12, 1], [18, 0.7]])
+MINIMA_DURATION_GENERATOR_ENVELOPE = core_events.Envelope(
+    [[0, 0.5], [4, 0.8], [12, 1], [14, 1], [18, 0.7]]
+)
 MINIMA_DURATION_GENERATOR_OFFSET = 1
 
-MAXIMA_DURATION_GENERATOR_ENVELOPE = core_events.Envelope([[0, 1], [30, 1], [60, 0.5]])
+MAXIMA_DURATION_GENERATOR_ENVELOPE = core_events.Envelope([[0, 0.65], [10, 1], [30, 1], [35, 0.9], [60, 0.5]])
 MAXIMA_DURATION_GENERATOR_OFFSET = 10
 
 PARTY_COUNT_TUPLE = (3, 4, 5)
